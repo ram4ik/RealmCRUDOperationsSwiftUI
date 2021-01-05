5a4ad1668644c9ecfd2723b1f40ddaa248ae36bf
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-01-05 12:48:15 +0200

    Initial Commit

---
+++ RealmCRUDOperationsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RealmCRUDOperationsSwiftUI
+//
+//  Created by ramil on 05.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RealmCRUDOperationsSwiftUI/RealmCRUDOperationsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmCRUDOperationsSwiftUIApp.swift
+//  RealmCRUDOperationsSwiftUI
+//
+//  Created by ramil on 05.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct RealmCRUDOperationsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
